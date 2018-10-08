e228f70eda1f003dcf62c707659f5ab274516b04
Jean-Marc Leoni <jean-marc.leoni@kametventures.com>
2018-10-08 11:55:35 +0200

    build(router): DIST of 6.0.3-router-fix-null-outlet

---
+++ esm2015/src/version.js
@@ -0,0 +1,14 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { Version } from '@angular/core';
+export const /** @type {?} */ VERSION = new Version('6.0.3-392a8546d');
+//# sourceMappingURL=version.js.map
---
+++ testing.d.ts
@@ -0,0 +1,6 @@
+/**
+ * @license Angular v6.0.3-392a8546d
+ * (c) 2010-2018 Google, Inc. https://angular.io/
+ * License: MIT
+ */ 
+ export * from './testing/testing'
